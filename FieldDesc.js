79c362234b5ae2680a46b264d47e75f5730d3284
Ariel Jakobovits <arieljake@yahoo.com>
2012-12-23 22:18:02 -0800

    initial import for form builder

---
+++ FieldDesc.js
@@ -0,0 +1,9 @@
+
+
+var FieldDesc = module.exports = function(label,name,type)
+{
+	this.label = label;
+	this.name = name;
+	this.type = type;
+};
+
---
+++ FieldGroupDesc.js
@@ -0,0 +1,9 @@
+
+
+var FieldGroupDesc = module.exports = function (label,name,type,fields)
+{
+	this.label = label;
+	this.name = name;
+	this.type = type;
+	this.fields = fields;
+}
