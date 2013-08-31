53525655863912e8379ae42c8a391f6eaaa9debe
Ariel Jakobovits <arieljake@yahoo.com>
2013-08-31 01:26:53 -0700

    added geolocation test

--- www/js/index.js
+++ www/js/index.js
@@ -45,5 +45,17 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+
+		navigator.geolocation.getCurrentPosition(function(pos)
+		{
+			alert('Latitude: '          + position.coords.latitude          + '\n' +
+				'Longitude: '         + position.coords.longitude         + '\n' +
+				'Altitude: '          + position.coords.altitude          + '\n' +
+				'Accuracy: '          + position.coords.accuracy          + '\n' +
+				'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+				'Heading: '           + position.coords.heading           + '\n' +
+				'Speed: '             + position.coords.speed             + '\n' +
+				'Timestamp: '         + position.timestamp                + '\n');
+		});
     }
 };
